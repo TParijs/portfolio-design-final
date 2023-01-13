dcf998a126fbe4e51bdf01925fe569a92280641c
TParijs <112637948+TParijs@users.noreply.github.com>
2023-01-13 16:36:58 +0100

    reworked final attempt 4.0

---
+++ js/index.js
@@ -0,0 +1,124 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector(".statusText");
+const restartBtn = document.querySelector(".restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    } 
+    const audio = new Audio("/img/click2.mp3");
+    audio.play();
+    updateCell(this, cellIndex);
+    checkWinner();
+    
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+    
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = ` ${currentPlayer}'s turn`;
+    
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+        
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        let source = "/img/wooo.mp3"
+    const audio = document.createElement("audio");
+    audio.autoplay = true;
+    audio.load()
+    audio.addEventListener("load", function() {
+    audio.play();
+    }, true);
+    audio.src = source;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+        let source = "/img/draw.mp3"
+    const audio = document.createElement("audio");
+    audio.autoplay = true;
+    audio.load()
+    audio.addEventListener("load", function() {
+    audio.play();
+    }, true);
+    audio.src = source;
+        running = false;
+
+    }
+    else{
+        changePlayer();
+        
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+    let source = "/img/reset1.mp3"
+const audio = document.createElement("audio");
+audio.autoplay = true;
+audio.volume = 0.5
+audio.load()
+audio.addEventListener("load", function() {
+  audio.play();
+}, true);
+audio.src = source;
+}
+
+let source = "/img/jazz.mp3"
+const audio = document.createElement("audio");
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 0.5
+audio.load()
+audio.addEventListener("load", function() {
+  audio.play();
+}, true);
+audio.src = source;
